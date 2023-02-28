2df6e82c8565161368590a1f2073b4a91df55257
JOEL <joelhappy16@gmail.com>
2023-02-28 02:15:41 +0100

    updt

---
+++ Golang/fichier.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main() {
+	var choixUtilisateur int
+
+	for {
+		fmt.Println("Menu:\n
+		1. Récupérer tout le texte contenu dans un fichier\n
+		2. Ajouter du texte dans un fichier\n
+		3. Supprimer tout le contenu d'un fichier\n
+		4. Remplacer le contenu d'un fichier par du texte donné par l'utilisateur\n
+		5. Quitter")
+
+		fmt.Print("Entrez votre choixUtilisateur: ")
+		fmt.Scan(&choixUtilisateur)
+
+		switch choixUtilisateur {
+		case 1:
+			lectureFichier()
+		case 2:
+			ajouterFichier()
+		case 3:
+			suppressionFichier()
+		case 4:
+			remplacerFichier()
+		case 5:
+			os.Exit(0)
+		default:
+			fmt.Println("ChoixUtilisateur invalide")
+		}
+	}
+}
+
+func lectureFichier() {
+	var nomFichier string
+
+	fmt.Print("Entrez le nom du fichier: ")
+	fmt.Scan(&nomFichier)
+
+	contenu, err := ioutil.ReadFile(nomFichier)
+	if err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier:", err)
+		return
+	}
+
+	fmt.Println("Contenu du fichier:")
+	fmt.Println(string(contenu))
+}
+
+func ajouterFichier() {
+	var nomFichier string
+	var texte string
+
+	fmt.Print("Entrez le nom du fichier: ")
+	fmt.Scan(&nomFichier)
+
+	fmt.Print("Entrez le texte à ajouter: ")
+	fmt.Scan(&texte)
+
+	fichier, err := os.OpenFile(nomFichier, os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println("Erreur lors de l'ouverture du fichier:", err)
+		return
+	}
+
+	defer fichier.Close()
+
+	_, err = fmt.Fprintln(fichier, texte)
+	if err != nil {
+		fmt.Println("Erreur lors de l'écriture dans le fichier:", err)
+		return
+	}
+
+	fmt.Println("Texte ajouté avec succès dans le fichier")
+}
+
+func suppressionFichier() {
+	var nomFichier string
+
+	fmt.Print("Entrez le nom du fichier: ")
+	fmt.Scan(&nomFichier)
+
+	err := os.Remove(nomFichier)
+	if err != nil {
+		fmt.Println("Erreur lors de la suppression du fichier:", err)
+		return
+	}
+
+	fmt.Println("Fichier supprimé avec succès")
+}
+
+func remplacerFichier() {
+	var nomFichier string
+	var texte string
+
+	fmt.Print("Entrez le nom du fichier: ")
+	fmt.Scan(&nomFichier)
+
+	fmt.Print("Entrez le texte de remplacement: ")
+	fmt.Scan(&texte)
+
+	err := ioutil.WriteFile(nomFichier, []byte(texte), 0644)
+	if err != nil {
+		fmt.Println("Erreur lors de la modification du fichier:", err)
+		return
+	}
+
+	fmt.Println("Contenu du fichier remplacé avec succès")
+}
